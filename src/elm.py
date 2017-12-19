664d88bd36d91fd5cb19bff76b475b44c82f2ddf
Renato Sousa <rsb5@cin.ufpe.br>
2017-12-19 10:02:35 -0300

    reorganizing the classes , creating the class for binary classification

--- src/elm.py
+++ src/elm.py
@@ -1,5 +1,6 @@
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.optimizers import Adam
 import numpy as np
 
 class ELM(object):
@@ -14,22 +15,21 @@ class ELM(object):
     	pass
 
     def setWeights(self):
-    	pass
+		weights = self.model.layers[-1].get_weights()
+		weights[0] = np.random.random_sample((weights[0].shape[0],weights[0].shape[1]))
+		self.model.layers[-1].set_weights(weights)
 
-    def compile(self):
-    	pass
+    def fit(self, X_train, y_train):
+		self.model.fit(X_train, y_train, verbose=2, batch_size=5, epochs=200)
 
-    def fit(self):
-    	pass
-
-    def evaluete(self):
-    	pass
+    def evaluete(self, X_test, y_test):
+		return self.model.evaluate(X_test, y_test)
 
     def getWeights(self):
-    	pass
+		return self.model.get_weights()
 
     def summary(self):
-    	pass
+		self.model.summary()
 
 class MultiClassify(ELM):
 
@@ -37,34 +37,41 @@ class MultiClassify(ELM):
 		self.model = Sequential()
 
 		# Input layer
-		self.model.add(Dense(units=10, activation=self.input_layer, input_dim=input_dim, trainable=True))
+		self.model.add(Dense(units=100, activation=self.input_layer, input_dim=input_dim, trainable=False))
+		self.setWeights()
 		# Hidden layer
 		for act in self.hidden_layer:
-			self.model.add(Dense(units=10, activation=act, trainable=False))
-			self.setWeights()
+			self.model.add(Dense(units=10, activation=act, trainable=True))
 		# Output layer
 		self.model.add(Dense(units=3, activation=self.output_layer, trainable=True))
 
 		self.model.compile(loss='categorical_crossentropy',
-              optimizer='sgd',
+              optimizer=Adam(lr=0.001),
               metrics=['accuracy'])
 
-	def setWeights(self):
-		weights = self.model.layers[-1].get_weights()
-		print(weights[1].shape)
-		weights[0] = np.random.random_sample((weights[0].shape[0],weights[0].shape[1]))
-		self.model.layers[-1].set_weights(weights)
+class BinaryClassify(ELM):
 
-	def fit(self, X_train, y_train):
-		self.model.fit(X_train, y_train, verbose=2, epochs=200)
+	def buildNetwork(self, input_dim):
+		self.model = Sequential()
 
-	def evaluete(self, X_test, y_test):
-		return self.model.evaluate(X_test, y_test)
+		# Input layer
+		self.model.add(Dense(units=110, activation=self.input_layer, input_dim=input_dim, trainable=False))
+		self.setWeights()
+		# Hidden layer
+		for act in self.hidden_layer:
+			self.model.add(Dense(units=10, activation=act, trainable=True))
+		# Output layer
+		self.model.add(Dense(units=2, activation=self.output_layer, trainable=True))
 
-	def getWeights(self):
-		return self.model.get_weights()
+		self.model.compile(loss='binary_crossentropy',
+              optimizer=Adam(lr=0.001),
+              metrics=['accuracy'])
 
-	def summary(self):
-		self.model.summary()
+
+	
+
+	
+
+	
 
 
