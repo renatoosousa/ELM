5ae66809fd6e98f6b5b98019ad303c166f2de8b7
Renato Sousa <rsb5@cin.ufpe.br>
2017-12-19 10:03:20 -0300

    re organize this example

--- examples/multiclassClassify.py
+++ examples/multiclassClassify.py
@@ -9,18 +9,18 @@ from elm import MultiClassify
 
 # Load data frame
 df = load_iris()
-X = df.data.astype(np.float32) 
-y = df.target.reshape(-1, 1).astype(np.int32)
+X = df.data 
+y = df.target.reshape(-1, 1)
 
 # One Hot encode the class labels
 encoder = OneHotEncoder(sparse=False)
 y = encoder.fit_transform(y)
 
 # Split data frame
-X_train, X_test, y_train, y_test = train_test_split(X, y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
 
 # Create classify
-elm = MultiClassify(hidden_layer=['relu','relu']) #two hidden layers
+elm = MultiClassify(hidden_layer=['relu'], output_layer='softmax') 
 
 input_dim = X.shape[1] # number of features
 elm.buildNetwork(input_dim)
